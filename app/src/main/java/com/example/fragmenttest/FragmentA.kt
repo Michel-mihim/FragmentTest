85d87b95029d2231dcaa6a984d1187f2233113e0
unknown <ya-mihim@yandex.ru>
2025-02-28 15:41:29 +0300

    fragment_a_by_arguments

--- app/src/main/java/com/example/fragmenttest/FragmentA.kt
+++ app/src/main/java/com/example/fragmenttest/FragmentA.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
 import com.example.fragmenttest.databinding.FragmentABinding
 
@@ -21,9 +22,20 @@ class FragmentA : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        val radio = requireArguments().getString(RADIO_KEY)
+        FragmentABinding.bind(view).fragmentA.text = radio
+
         // Добавляем первый вложенный фрагмент
         childFragmentManager.beginTransaction()
             .add(R.id.fragment_child_container, NestedFragmentA())
             .commit()
+    }
+
+    companion object {
+        const val RADIO_KEY = "RADIO_KEY"
+
+        fun newInstance(radio: String) = FragmentA().apply {
+            arguments = bundleOf(RADIO_KEY to radio)
+        }
     }
 }
--- app/src/main/java/com/example/fragmenttest/MainActivity.kt
+++ app/src/main/java/com/example/fragmenttest/MainActivity.kt
@@ -7,6 +7,9 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    val radio = "Radio 7"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -14,7 +17,7 @@ class MainActivity : AppCompatActivity() {
 
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
-                .add(R.id.fragment_container, FragmentA())
+                .add(R.id.fragment_container, FragmentA.newInstance(radio))
                 .commit()
         }
 
